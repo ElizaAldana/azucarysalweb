3b24feaa847bea5bfaf2c333fb7bb336ba1c7f41
Elizabeth Aldana Andrade <70041677+ElizaAldana@users.noreply.github.com>
2021-11-12 00:58:26 -0500

    firebase working but without sass

--- js/auth.js
+++ js/auth.js
@@ -1,12 +1,7 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-auth.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyCmZzvDPBNiYr_9V2lyzqFfzwPRzWeHERo",
     authDomain: "carrito-31765.firebaseapp.com",
     projectId: "carrito-31765",
@@ -14,8 +9,92 @@
     messagingSenderId: "740840766168",
     appId: "1:740840766168:web:826f902c85e2f3a444b5ad",
     measurementId: "G-SG7SYWCWW8"
-  };
+};
 
-  //Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+//Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+
+
+const registerForm = document.getElementById("register");
+const loginForm = document.getElementById("ingresar");
+
+const createUser = async (email, password) => {
+    try {
+        await createUserWithEmailAndPassword(auth, email, password)
+    } catch (e) {
+        if (e.code === "auth/email-already-in-use") {
+            alert("Ups...el correo que ingresaste ya está en uso");
+        }
+        if (e.code === "auth/weak-password") {
+            alert("Oh no, contraseña débil :(");
+        }
+    }
+}
+
+registerForm.addEventListener("submit", e => {
+    e.preventDefault();
+    e.preventDefault();
+    const name = registerForm.name.value;
+    const email = registerForm.email.value;
+    const password = registerForm.password.value;
+
+    if (email && password) {
+        createUser(email, password);
+    } else {
+        alert();
+    }
+});
+
+const login = async (email, password) => {
+    try {
+        const user = await signInWithEmailAndPassword(auth, email, password);
+        console.log(user);
+    } catch (e) {
+        console.log(e.code);
+        if(e.code === "auth/user-not-found"){
+            alert("Pon correo real");
+        }
+        if(e.code === "auth/wrong-password"){
+            alert("Pon contraseña real");
+        }
+    }
+}
+
+loginForm.addEventListener("submit", e => {
+    e.preventDefault();
+    const email = loginForm.email.value;
+    const password = loginForm.password.value;
+
+    if (email && password) {
+        login(email, password);
+    } else {
+        console.log("Completa todos los datos");
+    }
+});
+
+const logOut = async () => {
+    try {
+        await signOut(auth);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+
+const logOutButton = document.getElementById("logOut");
+
+logOutButton.addEventListener("click", () =>{
+    logOut();
+} )
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        loginForm.classList.add("hidden");
+        logOutButton.classList.add("visible");
+    } else {
+        loginForm.classList.remove("hidden");
+        logOutButton.classList.remove("visible");
+    }
+  });
